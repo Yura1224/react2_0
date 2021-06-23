ccb43a2bb39acef457924d6823b75e47994d18fb
Yuriy1223 <yuramoskva28@gmail.com>
2021-06-23 18:56:28 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 
-function App() {
+
+export default function App() {
     return (
-        <div className="App">
-            ffffffffffffffffffffff
+        <div>
+
         </div>
     );
 }
 
-export default App;
+
+
+
