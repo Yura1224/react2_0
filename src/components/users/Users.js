d3167b7f30d62c51b9bdf4260533c6cfb74cf626
Yuriy1223 <yuramoskva28@gmail.com>
2021-06-23 20:33:38 +0300

    initial

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,19 +1,26 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 
 export default function Users(props) {
+
+
+    const [usersArray, setUsersArray] = useState([]);
+
     useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users/")
-        // .then(value => console.log(value))
-        .then(value => value.json())
-        .then(usersfromServer => {
-            console.log(usersfromServer);
-        });
-}, []);
+        fetch("https://jsonplaceholder.typicode.com/users/")
+            // .then(value => console.log(value))
+            .then(value => value.json())
+            .then(usersfromServer => {
+                console.log(usersfromServer);
+                setUsersArray(usersfromServer);
+            });
+    }, []);
 
     return (
         <div>
-           
+            {
+                usersArray.map(user => <div key={user.id}>{user.id} - {user.name}</div>)
+            }
         </div>
     );
 }
