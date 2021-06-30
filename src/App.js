99f106d71b82599d83508bb8b1e60020ededfc55
Yuriy1223 <yuramoskva28@gmail.com>
2021-06-30 20:12:36 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Users from "./Lesson3/Components/Users/Users";
+import Posts from "./Lesson3/Components/Posts/Posts";
+import Comments from "./Lesson3/Components/Comments/Comments";
 
-import {Router, Link} from "react-router-dom";
 
 
 export default function App() {
@@ -9,12 +18,29 @@ export default function App() {
     return (
         <Router>
             <div className={"App"}>
+                <ul>
+                    <li>
+                        <Link to={"/"}>Home </Link>
+                    </li>
+                    <li>
+                        <Link to={"/users"}>Users</Link>
+                    </li>
+                    <li>
+                        <Link to={"/posts"}>Posts</Link>
+                    </li>
+                    <li>
+                        <Link to={"/comments"}>Comments</Link>
+                    </li>
+                    <Switch>
+                        <Route exact={true} path={"/"}>
+                            <Users/>
+                        </Route>
+                        <Route path={"/users"} component={Users}/>
 
-
-                <Link>
-                    Home
-                </Link>
-
+                        <Route path={"/posts"} component={Posts}/>
+                        <Route path={"/comments"} render={() => <Comments/>}/>
+                    </Switch>
+                </ul>
             </div>
 
 
--- src/Lesson3/Components/Users/Users.js
+++ src/Lesson3/Components/Users/Users.js
@@ -1,2 +1,21 @@
+import {useEffect, useState} from "react";
+import User from "../User/User";
 
 
+export default function Users() {
+    let [users,setUsers] = useState([]);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com")
+            .then(value => value.json())
+            .then(value => {
+                setUsers(value);
+            });
+    }, []);
+    return (
+        <div>
+            users.map(value => <User key={value.id}
+        item={value}
+        />)
+        </div>
+    );
+}
